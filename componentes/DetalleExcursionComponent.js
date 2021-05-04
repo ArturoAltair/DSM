c3183fe04db85719a0107f2d6a427ee141d76df9
ArturoAltair <altairarturo@gmail.com>
2021-05-04 13:46:12 +0200

    Redux y Thunk

--- componentes/DetalleExcursionComponent.js
+++ componentes/DetalleExcursionComponent.js
@@ -4,6 +4,14 @@ import { COMENTARIOS } from '../comun/comentarios';
 import { Card, Icon } from 'react-native-elements';
 import { EXCURSIONES } from '../comun/excursiones';
 import { baseUrl } from '../comun/comun';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+    return {
+        excursiones: state.excursiones,
+        comentarios: state.comentarios
+    }
+}
 
 function RenderComentario(props) {
   const comentarios = props.comentarios;
@@ -65,8 +73,8 @@ class DetalleExcursion extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      excursiones: EXCURSIONES,
-      comentarios: COMENTARIOS,
+      // excursiones: EXCURSIONES,
+      // comentarios: COMENTARIOS,
       favoritos: []
     };
   }
@@ -82,16 +90,16 @@ class DetalleExcursion extends Component {
     return (
       <ScrollView>
         <RenderExcursion
-          excursion={this.state.excursiones[+excursionId]}
+          excursion={this.props.excursiones.excursiones[+excursionId]}
           favorita={this.state.favoritos.some(el => el === excursionId)}
           onPress={() => this.marcarFavorito(excursionId)}
         />
         <RenderComentario
-          comentarios={this.state.comentarios.filter((comentario) => comentario.excursionId === excursionId)}
+          comentarios={this.props.comentarios.comentarios.filter((comentario) => comentario.excursionId === excursionId)}
         />
       </ScrollView>
     );
   }
 }
 
-export default DetalleExcursion;
+export default connect(mapStateToProps)(DetalleExcursion);
