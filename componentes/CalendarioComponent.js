6ba68c36d0a8ff3b112b97682a75e92e46d14a06
ArturoAltair <altairarturo@gmail.com>
2021-05-11 15:41:42 +0200

    Activity Indicator y addFavoritos

--- componentes/CalendarioComponent.js
+++ componentes/CalendarioComponent.js
@@ -1,45 +1,75 @@
 import React, { Component } from 'react';
 import { ListItem, Avatar } from 'react-native-elements';
-import { SafeAreaView, FlatList } from 'react-native';
+import { SafeAreaView, FlatList, Text, View } from 'react-native';
+import { IndicadorActividad } from './IndicadorActividadComponent';
 import { baseUrl } from '../comun/comun';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
     return {
-      excursiones: state.excursiones
+        excursiones: state.excursiones
     }
-  }
+}
+
+// function FlatList(props) {
+//     if (props.isLoading) {
+//         return (
+//             <IndicadorActividad />
+//         );
+//     }
+//     else if (props.errMess) {
+//         return (
+//             <View>
+//                 <Text>{props.errMess}</Text>
+//             </View>
+//         );
+//     }
+// }
 
 class Calendario extends Component {
 
-    render(){
+    render() {
 
-    const { navigate } = this.props.navigation;    
+        const { navigate } = this.props.navigation;
 
-    const renderCalendarioItem = ({item, index}) => {
-        return (
-            <ListItem
-                key={index}
-                onPress={() => navigate('DetalleExcursion', { excursionId: item.id })}
-                bottomDivider>
-                <Avatar source={{uri: baseUrl + item.imagen}} />
-                <ListItem.Content>
-                    <ListItem.Title>{item.nombre}</ListItem.Title>
-                    <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle>
-                </ListItem.Content>
-            </ListItem> 
-        );
-    };
+        const renderCalendarioItem = ({ item, index }) => {
+            return (
+                <ListItem
+                    key={index}
+                    onPress={() => navigate('DetalleExcursion', { excursionId: item.id })}
+                    bottomDivider>
+                    <Avatar source={{ uri: baseUrl + item.imagen }} />
+                    <ListItem.Content>
+                        <ListItem.Title>{item.nombre}</ListItem.Title>
+                        <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
+            );
+        };
 
-    return (
-        <SafeAreaView>
-            <FlatList 
-                data={this.props.excursiones.excursiones}
-                renderItem={renderCalendarioItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </SafeAreaView>
-    );
+        if (this.props.excursiones.isLoading) {
+            return (
+                <IndicadorActividad />
+            );
+        }
+        else if (this.props.excursiones.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.errMess}</Text>
+                </View>
+            );
+        } else {
+
+            return (
+                <SafeAreaView>
+                    <FlatList
+                        data={this.props.excursiones.excursiones}
+                        renderItem={renderCalendarioItem}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                </SafeAreaView>
+            );
+        }
     }
 }
 
