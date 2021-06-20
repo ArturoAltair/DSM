5c705b9050947e61a509c1c4fa7ec13e7583c03e
ArturoAltair <altairarturo@gmail.com>
2021-06-20 21:33:19 +0200

    Configurar backend Firebase upload: comentarios

--- redux/comentarios.js
+++ redux/comentarios.js
@@ -1,18 +1,34 @@
+import { baseUrl } from '../comun/comun';
 import * as ActionTypes from './ActionTypes';
 
-export const comentarios = (state = { errMess: null, comentarios:[]}, action) => {
+export const comentarios = (state = { errMess: null, comentarios: [] }, action) => {
   switch (action.type) {
     case ActionTypes.ADD_COMENTARIOS:
-      return {...state, errMess: null, comentarios: action.payload};
+      return { ...state, errMess: null, comentarios: action.payload };
 
     case ActionTypes.COMENTARIOS_FAILED:
-      return {...state, errMess: action.payload};
+      return { ...state, errMess: action.payload };
 
-      case ActionTypes.ADD_COMENTARIO:
-       var com = action.payload;
-       com.id = state.comentarios.length;
-       return {...state, errMess: null, comentarios: state.comentarios.concat(com)};
-        
+    case ActionTypes.ADD_COMENTARIO:
+      var com = action.payload;
+      var mensajeError = null;
+      com.id = state.comentarios.length;
+      //  return {...state, errMess: null, comentarios: state.comentarios.concat(com)};
+      fetch(baseUrl + 'comentarios/' + com.id + '.json', {
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+          com
+        )
+      })
+        .then(response => response.json())
+        .catch(error => { mensajeError = error.message });
+
+      return { ...state, errMess: mensajeError, comentarios: state.comentarios.concat(com) };
+
     default:
       return state;
   }
